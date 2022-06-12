ad55604889354b9a91e9e2321bf89331782060b8
Artiom Kalinklin <artem.k.3008@gmail.com>
2022-06-12 17:19:49 +0300

    initial commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Majesty Developer</title>
+  <link rel="stylesheet" href="/public/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/public/css/style.css">
+</head>
+
+<body>
+<header>
+    <?php 
+     echo require 'application/lib/header.php';
+    ?>
+    <div class="time"></div>
+  </header>
+
+  <nav>
+    <ul class="content__links">
+
+    </ul>
+  </nav>
+
+
+<section class="content container">
+   
+</section>
+
+  
+</body>
+<script src="/public/scripts/jquery-3.6.0.min.js"></script>
+<script src="/public/scripts/script.js"></script>
+
+</html>
